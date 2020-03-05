f9013cd20cc23e0aa6a8372ff750adc33e54da4f
thecreatorslab <elivelton.oliveira@thecreators.com.br>
2020-03-05 16:52:04 -0300

    first commit

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div>
         <Hello name={this.state.name} />
         <p>
-          Start editing to see some magic happen :)
+          Hello Uordi
         </p>
       </div>
     );
